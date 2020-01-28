b19c6aa01be2995fcd6d9c7e27edaae9b8b72763
Alexandre Palota <alexandre.palota@gmail.com>
2020-01-27 21:25:28 -0300

    Implementando o Preço

--- src/br/com/casadocodigo/loja/models/Produto.java
+++ src/br/com/casadocodigo/loja/models/Produto.java
@@ -1,5 +1,8 @@
 package br.com.casadocodigo.loja.models;
 
+import java.util.List;
+
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -14,6 +17,8 @@ public class Produto {
 	private String titulo;
 	private String descricao;
 	private int paginas;
+	@ElementCollection
+	private List<Preco> precos;
 
 	public String getTitulo() {
 		return titulo;
@@ -52,5 +57,13 @@ public class Produto {
 	@Override
 	public String toString() {
 		return "Produto [titulo=" + titulo + ", descricao=" + descricao + ", paginas=" + paginas + "]";
+	}
+
+	public List<Preco> getPrecos() {
+		return precos;
+	}
+
+	public void setPrecos(List<Preco> precos) {
+		this.precos = precos;
 	}
 }
